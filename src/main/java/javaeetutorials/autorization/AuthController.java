8132460427cf31145ca1b7d466faa37157ad211c
stasiukevych <stasiukevych.a@gmail.com>
2023-09-28 05:55:34 -0500

    make authController as EJB, and set up roles though runAs.

--- src/main/java/javaeetutorials/autorization/AuthController.java
+++ src/main/java/javaeetutorials/autorization/AuthController.java
@@ -1,14 +1,19 @@
 package javaeetutorials.autorization;
 
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RunAs;
 import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 import java.security.Principal;
 
-@RunAs("user")
+@Stateless
+//@RunAs("user")
+@RunAs("admin")
 @Path("authController")
 public class AuthController {
 
@@ -20,7 +25,24 @@ public class AuthController {
 
   @GET
   @Path("all")
+  @PermitAll
   public Response getAll() {
     return Response.ok().entity(service.findUsers()).build();
+  }
+
+  @GET
+  @Path("create/{userName}")
+  @PermitAll
+  public Response addUserName(@PathParam("userName") String userName) {
+    service.addUserName(userName);
+    return Response.ok().entity("was added: " + userName).build();
+  }
+
+  @GET
+  @Path("delete/{userName}")
+  @PermitAll
+  public Response delete(@PathParam("userName") String userName) {
+    service.deleteUserName(userName);
+    return Response.ok().entity("was removed: " + userName).build();
   }
 }
